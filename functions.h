3a68d213ecef54f231e7552e018ba9eb985154f6
vasilisavasileva <55589269+vasilisavasileva@users.noreply.github.com>
2020-08-08 11:42:26 +0300

    Delete functions.h

--- functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<Windows.h>
-#include<string>
-
-const std::string PROCCESS_B = "B";			//название программы B
-const std::string PROCCESS_C = "C";			//название программы C
-HANDLE CreateProcessB();
-HANDLE CreateProcessC();
-HANDLE CreateNamedProcess(const std::string& name);
